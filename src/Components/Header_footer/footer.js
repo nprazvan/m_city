2545ad1230327156a4f93d278f23a8d4e3872520
nprazvan <razvan_nicolaie@yahoo.com>
2023-06-02 20:12:38 +0300

    added footer and logo

--- src/Components/Header_footer/footer.js
+++ src/Components/Header_footer/footer.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
+import { CityLogo } from "../Utils/tools";
 
 const Footer = () => {
-    return(
-        <>
-            Footer
-        </> 
-    )
-}
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo link={true} linkTo={"/"} width="70px" height="70px" />
+      </div>
+      <div className="footer_descl">
+        Manchester City 2023.All rights reserved.
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/Components/Header_footer/header.js
+++ src/Components/Header_footer/header.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import { AppBar, Toolbar, Button } from '@material-ui/core'
+import React from "react";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import { CityLogo } from "../Utils/tools";
 
 const Header = () => {
-    return(
-        <AppBar
-            position="fixed"
-            style={{
-                backgroundColor:'#98c5e9',
-                boxShadow:'none',
-                paddin:'10px 0',
-                borderBottom:'2px solid #00285e'
-            }}
-        >
-            <Toolbar style={{ display:'flex' }}>
-                <div style={{ flexGrow: 1 }}>
-                    <div className="header_logo">
-                        logo
-                    </div>
-                </div>
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: "#98c5e9",
+        boxShadow: "none",
+        paddin: "10px 0",
+        borderBottom: "2px solid #00285e",
+      }}
+    >
+      <Toolbar style={{ display: "flex" }}>
+        <div style={{ flexGrow: 1 }}>
+          <div className="header_logo">
+            <CityLogo link={true} linkTo={"/"} width="70px" height="70px" />
+          </div>
+        </div>
 
-                <Link to="/the_team">
-                    <Button color="inherit">The team</Button>
-                </Link>
-                <Link to="/the_matches">
-                    <Button color="inherit">Matches</Button>
-                </Link>
+        <Link to="/the_team">
+          <Button color="inherit">The team</Button>
+        </Link>
+        <Link to="/the_matches">
+          <Button color="inherit">Matches</Button>
+        </Link>
 
-                <Link to="/dashboard">
-                    <Button color="inherit">Dashboard</Button>
-                </Link>
-
-            </Toolbar>
-        </AppBar> 
-    )
-}
+        <Link to="/dashboard">
+          <Button color="inherit">Dashboard</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
